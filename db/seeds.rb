5aedb81d687c13ef4390846d38d543e23fb6734f
Reza Jatnika <rezajatnika@gmail.com>
2015-11-15 22:55:39 +0700

    Fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,38 @@
-# Student
-User.create(
-  username: 'student',
-  email: 'student@example.com',
-  password: 'asdqwe123',
-  password_confirmation: 'asdqwe123'
-  active: true
-  )
+# Students
+30.times do |i|
+  User.create(
+    username: "student-#{i}",
+    email: "student-#{i}@example.com",
+    password: 'asdqwe123',
+    password_confirmation: 'asdqwe123',
+    active: true
+    )
+end
 
-# Lecturer
-User.create(
-  username: 'student',
-  email: 'student',
-  password: 'asdqwe123',
-  password_confirmation: 'asdqwe123'
-  active: true
-  role: 'lecturer'
+# Lecturers
+5.times do |i|
+  User.create(
+    username: "lecturer-#{i}",
+    email: "lecturer-#{i}@example.com",
+    password: 'asdqwe123',
+    password_confirmation: 'asdqwe123',
+    active: true,
+    role: 'lecturer'
+    )
+end
+
+# Courses
+lecturer = User.where(role: 'lecturer').first
+
+lecturer.courses.create(
+  name: 'Introduction to Computer Science',
+  code: 'cs-101'
+  )
+lecturer.courses.create(
+  name: 'Programming Abstraction',
+  code: 'cs-201'
+  )
+lecturer.courses.create(
+  name: 'Programming Paradigms',
+  code: 'cs-202'
   )
